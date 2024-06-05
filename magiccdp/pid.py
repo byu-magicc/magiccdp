50c08c278c5ae95c610e5d7b264fcda3aa7eadc7
James Usevitch <james_usevitch@byu.edu>
2024-06-05 02:47:47 -0600

    Gradient descent still not working

--- magiccdp/pid.py
+++ magiccdp/pid.py
@@ -52,7 +52,8 @@ class PIDSystem(eqx.Module):
         # pid_num = jnp.array([kp*Tf + kd, kp + ki*Tf, ki])
         # pid_denom = jnp.array([Tf, 1.0, 0])
 
-        pid_num = jnp.array([kd, kp, ki])
+        # pid_num = jnp.array([kd, kp, ki])
+        pid_num = jnp.hstack([kd, kp, ki])
         pid_denom = jnp.array([1.0, 0])
 
         dyn_num = jnp.array(self.dyn_num)
@@ -108,7 +109,7 @@ class PIDSystem(eqx.Module):
         return A @ x + B * params['ref']
 
 
-def solve(system: PIDSystem, x0: Array, ref: float, t1=1.0):
+def solve(system: PIDSystem, x0: Array, ref: float, t1=1.0, resolution=1000):
     terms = ODETerm(system)
     sol = diffeqsolve(
         terms=terms,
@@ -118,22 +119,24 @@ def solve(system: PIDSystem, x0: Array, ref: float, t1=1.0):
         dt0=0.01,
         y0=x0,
         args={'ref': ref},
-        saveat=SaveAt(ts=jnp.linspace(0.0, t1, 1000)),
+        saveat=SaveAt(ts=jnp.linspace(0.0, t1, resolution)),
         max_steps=10000,
         # stepsize_controller=PIDController(rtol=1e-5, atol=1e-5),
     )
     return sol
 
 
-def make_loss(system):
+def make_loss(system, t1=1.0, resolution=100):
 
-    A, B, C, D = system._statespace()
 
     @eqx.filter_value_and_grad
     def loss(system, x0, ref):
-        sol = solve(system, x0, ref)
+        sol = solve(system, x0, ref, t1=t1, resolution=resolution)
+        _, _, C, D = system._statespace()
         y = (C @ sol.ys.T).T + D * ref
-        return jnp.sum((y - ref)**2)
+        loss = jnp.sum((y - ref)**2)
+        jax.debug.breakpoint()
+        return loss
 
     return loss
     
@@ -158,11 +161,14 @@ if __name__ == "__main__":
     l = 0.3
     g = 9.8
 
+    T1 = 60.0
+    RESOLUTION = 500
+
     single_arm = PIDSystem(
-        kp=0.01,
-        ki=0.01,
+        kp=jnp.array([0.01]),
+        ki=jnp.array([0.01]),
         # kd=0.095,
-        kd=0.1,
+        kd=jnp.array([0.1]),
         # Tf=1.0,
         dyn_num=[3/(m*l**2)],
         dyn_denom=[1.0, 3*b/(m*l**2), 0.0]
@@ -174,7 +180,7 @@ if __name__ == "__main__":
 
     pdb.set_trace() if sys.flags.debug else None
 
-    sol = solve(single_arm, x0, ref, t1=60.0)
+    sol = solve(single_arm, x0, ref, t1=T1, resolution=RESOLUTION)
 
     y = (C @ sol.ys.T).T + D * ref
 
@@ -184,20 +190,27 @@ if __name__ == "__main__":
     plt.show()
 
 
-    if False:
+    if True:
 
-        lr = 1e-3
+        lr = 1e-5
         opt = optax.sgd(learning_rate=lr)
         opt_state = opt.init(single_arm)
 
-        loss = make_loss(single_arm)
+        loss = make_loss(single_arm, t1=T1, resolution=500)
         step_fn = make_step(opt, loss)
 
 
         pdb.set_trace() if sys.flags.debug else None
 
-        for ii in range(100):
+        for ii in range(5000):
             value, single_arm, opt_state = step_fn(single_arm, opt_state, x0, ref)
-            print(f"Loss at Step {ii}: {value}")
+            if ii % 10 == 0:
+                print(f"Loss at Step {ii}: {value}")
     
+        sol = solve(single_arm, x0, ref, t1=60.0)
+        y = (C @ sol.ys.T).T + D * ref
+
+        plt.plot(sol.ts, y)
+        plt.plot(sol.ts, jnp.ones_like(sol.ts) * ref)
+        plt.show()
 
