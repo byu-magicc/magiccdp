148fc976ecf0a9e6ab2a58e8e28a7501b1295a17
James Usevitch <james_usevitch@byu.edu>
2024-06-04 20:57:14 -0600

    Before refactor

--- magiccdp/pid.py
+++ magiccdp/pid.py
@@ -16,26 +16,46 @@ class PIDController(eqx.Module):
     ki: Array | float
     kd: Array | float
     Tf: Array | float # For approximation of derivative TF; see https://www.cds.caltech.edu/~murray/courses/cds101/fa04/caltech/am04_ch8-3nov04.pdf
-    # dyn_num: list[float]
-    # dyn_denom: list[float]
+    dyn_num: list[float]
+    dyn_denom: list[float]
 
-    def _numerator(self):
+    def __init__(self,*,
+        kp: Array | float,
+        ki: Array | float,
+        kd: Array | float,
+        Tf: Array | float,
+        dyn_num: list[float],
+        dyn_denom: list[float]
+    ):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.Tf = Tf
+        self.dyn_num = dyn_num
+        self.dyn_denom = dyn_denom
+
+    def _num_denom(self):
         kp = self.kp
         ki = self.ki
         kd = self.kd
         Tf = self.Tf
-        return jnp.array([kp*Tf + kd, kp + ki*Tf, ki])/Tf
 
-    def _denominator(self):
-        Tf = self.Tf
-        return jnp.array([1, 1/Tf, 0])
+        pid_num = jnp.array([kp*Tf + kd, kp + ki*Tf, ki])
+        pid_denom = jnp.array([Tf, 1.0, 0])
+
+        denom = jnp.polymul(pid_denom, self.dyn_denom)
+        lead_coeff = denom[0]
+        denom = denom/lead_coeff
+        num = jnp.polymul(pid_num, self.dyn_num) / lead_coeff
+
+        return num, denom
+        
 
     def _statespace(self):
         """
         Returns the state-space representation of the PID controller.
         """
-        denom = self._denominator()
-        num = self._numerator()
+        num, denom = self._num_denom()
         D, num = jnp.polydiv(num, denom)
 
         A = jnp.vstack(
@@ -49,5 +69,9 @@ class PIDController(eqx.Module):
         return A, B, C, D
 
 
-    def __call__(self, x):
-        pass
+    def __call__(self, x, params):
+        A, B, C, D = self._statespace()
+
+        err = params['ref'] - x
+
+        return A @ x + B @ err
